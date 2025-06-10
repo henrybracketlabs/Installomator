6ae700a871bcffe9bf0f3d503a3f84a2041906be
henrybracketlabs <henry.vu@bracketlabs.com>
2025-06-10 14:51:57 -0700

    Fix sparklefeed data

--- fragments/labels/zight.sh
+++ fragments/labels/zight.sh
@@ -1,8 +1,8 @@
 zight)
     name="Zight"
     type="zip"
-    sparkleData=$(curl -fsL 'https://share.zight.com/api/v4/clients/mac/current-version')
-    appNewVersion=$( <<<"$sparkleData" xpath 'string(//item[last()]/sparkle:shortVersionString)' )
-    downloadURL=$( <<<"$sparkleData" xpath 'string(//item[last()]/enclosure/@url)' )
+    sparkleFeed=$(curl -fsL 'https://share.zight.com/api/v4/clients/mac/current-version')
+    appNewVersion="$(echo $sparkleFeed | xpath 'string(//rss/channel/item/enclosure/@sparkle:shortVersionString)' 2>/dev/null)"
+    downloadURL="$(echo $sparkleFeed | xpath 'string(//rss/channel/item/enclosure/@url)' 2>/dev/null)"
     expectedTeamID="DCMDDJEW9X"
     ;;
