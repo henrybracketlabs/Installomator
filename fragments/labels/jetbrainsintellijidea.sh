9b83e0890b786894e46be98edf906314013d8834
henrybracketlabs <78557537+henrybracketlabs@users.noreply.github.com>
2026-01-05 10:37:52 -0800

    Rebase 1/5/2026

--- fragments/labels/jetbrainsintellijidea.sh
+++ fragments/labels/jetbrainsintellijidea.sh
@@ -11,6 +11,7 @@ intellijideace)
     fi
     blockingProcesses=( "idea" )
     downloadURL="https://download.jetbrains.com/product?code=${jetbrainscode}&latest&distribution=${jetbrainsdistribution}"
-    appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "location" | tail -1 | sed -E 's/.*-([0-9.]+)[-.].*/\1/g' )
+    updateData=$(curl -fsL 'https://www.jetbrains.com/updates/updates.xml')
+    appNewVersion=$( <<<"$updateData" xpath "string(//product[@name='IntelliJ IDEA']//channel[@id='IC-IU-RELEASE-licensing-RELEASE']//build[1]/@version)" )
     expectedTeamID="2ZEFAR8TH3"
     ;;
